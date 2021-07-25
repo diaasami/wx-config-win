b46781140bbdf8f2b62dd6e1982cc0afcd82f832
Diaa Sami <diaasami@gmail.com>
2021-07-26 00:13:04 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-/mingw32/bin/i686-w64-mingw32-g++ -o wx-config wx-config-win.cpp
+/mingw32/bin/i686-w64-mingw32-g++ -Os -o wx-config $GITHUB_WORKSPACE/wx-config-win.cpp
+strip $GITHUB_WORKSPACE/wx-config.exe
