ce24fc1582be220e29c714f039a648050684cefa
Diaa Sami <diaasami@fastmail.fm>
2022-03-17 14:16:02 +0100

    Update build.sh
    -static-libgcc

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 /mingw32/bin/i686-w64-mingw32-g++ -Os -o wx-config $GITHUB_WORKSPACE/wx-config-win.cpp
+/mingw32/bin/i686-w64-mingw32-g++ -Os -static-libgcc -o wx-config $GITHUB_WORKSPACE/wx-config-win.cpp
 strip $GITHUB_WORKSPACE/wx-config.exe
