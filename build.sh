f606847a6f7eaf5c13741983bb8c22ebaad8780a
Diaa Sami <diaasami@fastmail.fm>
2022-03-17 15:18:13 +0100

    build.sh: avoid dependency on libstdc++ dll

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-/mingw32/bin/i686-w64-mingw32-g++ -Os -static-libgcc -o wx-config $GITHUB_WORKSPACE/wx-config-win.cpp
+/mingw32/bin/i686-w64-mingw32-g++ -Os -static-libgcc -static-libstdc++ -o wx-config $GITHUB_WORKSPACE/wx-config-win.cpp
 strip $GITHUB_WORKSPACE/wx-config.exe
